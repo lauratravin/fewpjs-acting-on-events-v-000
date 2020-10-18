23ac48960ac4584185475d65673986e67093e737
Laura Messa Travin <laura.travin@gmail.com>
2020-10-18 00:37:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,19 @@ function moveDodgerLeft(){
     dodger.style.left = `${left - 1}px`;
   }
 }
+function moveDodgerRight(){
+  let rightNumbers = dodger.style.right.replace("px","");
+  let right = parseInt( rightNumbers , 10);
+  if (right > 0){
+    dodger.style.right = `${right+1}px`;
+  }
+}
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  }
+  if (e.key ==  "ArrowRight"){
+    moveDodgerRight();
   }
 });
